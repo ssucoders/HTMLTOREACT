81c3dd4c03b6a2bbb6eb254be7f2d1e80aab9956
mUbaidAwan <uawan2385@gmail.com>
2019-09-09 20:49:45 +0500

    added header html

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,71 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Header extends Component{
-    constructor(props){
+class Header extends Component {
+    constructor(props) {
         super(props);
     }
-    render(){
+    render() {
         return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <a className="navbar-brand" href="#">Team Manager</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <form className="form-inline my-2 my-lg-0 ml-auto">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                </form>
-            </div>
-    </nav>
+            <React.Fragment>
+                <div id="topHeader">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-5 ">
+                                <a href="#">companyinfo@gmail.com</a>
+                                <a href="#">2AO.Queenstown St.USA</a>
+                            </div>
+                            <div className="col-2"></div>
+                            <div className="col-5 text-right">
+                                <p> <a href="#">Follow Us </a><span>Call Us :</span> 000 888 999 </p>
+                            </div>
+                        </div>
+                        <div className="clear"></div>
+                    </div>
+                </div>
+
+
+
+                <header>
+
+                    <div className="container">
+                        <nav className="navbar navbar-expand-lg navbar-light">
+                            <a className="navbar-brand " href="#" id="logo" title="Finconix">Finconix</a>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item ">
+                                        <a className="nav-link" href="#">Home </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">About</a>
+                                    </li>
+                                    <li className="nav-item ">
+                                        <a className="nav-link" href="#">Services</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Project</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Blog</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Page</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Contact</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a href="#" className="btn btn-primary">Get a Quotes</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div className="clear"></div>
+                    </div>
+                </header>
+            </React.Fragment>
         )
     }
 }
