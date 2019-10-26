f6a5f6b438b5e500f622c8e3fc7d822b06802ffd
mUbaidAwan <uawan2385@gmail.com>
2019-10-26 19:32:25 +0500

    change text in services and set statistics bar

--- src/components/serviceCard.js
+++ src/components/serviceCard.js
@@ -19,7 +19,7 @@ class ServicesCard extends Component {
                                 <div className="card-body">
                                     <h5 className="card-title">{data.title}</h5>
                                     <h6 className="card-subtitle mb-2 text-muted">{data.subtitle}</h6>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p className="card-text">{data.text}</p>
                                     {services.map((el , i)=>{
                                         return(<ServicesButton data={el} key={i} />)
                                     })}
--- src/components/servicesListing.js
+++ src/components/servicesListing.js
@@ -7,9 +7,9 @@ class ServicesListing extends Component {
     }
     render() {
         let services = [
-            {"title":"Financial Planning", "subtitle": "Best Analysis", "image": "image.jpg",},
-            {"title":"Corporate Services", "subtitle": "Analysis Finance", "image": "IMG-20180915-WA0014.jpg"},
-            {"title":"Busniess Consulting", "subtitle": "Free Consulting", "image": "IMG-20181003-WA0022.jpg"}
+            {"title":"Financial Planning", "subtitle": "Best Analysis", "image": "image.jpg", "text":"Some quick example text to build on the card title and make up the bulk of the card's content."},
+            {"title":"Corporate Services", "subtitle": "Analysis Finance", "image": "IMG-20180915-WA0014.jpg","text":"Some quick example text to build on the card title and make up the bulk of the card's content."},
+            {"title":"Busniess Consulting", "subtitle": "Free Consulting", "image": "IMG-20181003-WA0022.jpg","text":"Some quick example text to build on the card title and make up the bulk of the card's content."}
         ]
         return (
             <React.Fragment>
